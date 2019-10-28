04a161855c5f7bfcadb4681ad903a33c958e877c
Mateus <mateuslima020@outlook.com>
2019-10-28 00:08:27 -0300

    initial set up

---
+++ src/app.js
@@ -0,0 +1,13 @@
+import express from 'express';
+
+class App {
+  constructor() {
+    this.server = express();
+
+    this.middlewares();
+  }
+
+  middlewares() {
+    this.server.use(express.json());
+  }
+}
